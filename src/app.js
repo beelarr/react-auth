e8b443b783273167e12c7174f4613efe37bd68bb
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-09-12 16:29:30 -0500

    Added inputs for authentication with functionality and styling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import {Header} from './Components/common';
+import firebase from 'firebase';
+import LoginForm from './Components/LoginForm';
+
 import {
     View,
     Text
@@ -8,11 +11,21 @@ from 'react-native';
 
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyA1zCifu0tKqg2Hlz7OcnDlRZXw8Q5FGUk",
+            authDomain: "donut-test-data.firebaseapp.com",
+            databaseURL: "https://donut-test-data.firebaseio.com",
+            projectId: "donut-test-data",
+            storageBucket: "donut-test-data.appspot.com",
+            messagingSenderId: "673089384537"
+        });
+    }
     render() {
         return (
             <View>
                 <Header headerText="Authentication"/>
-                <Text>An App!</Text>
+                <LoginForm/>
             </View>
         )
     }
